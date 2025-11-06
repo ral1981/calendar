56c1f145ab52623005a1526efadd31bf95f8969f
ral1981 <rolfalsemgeest@gmail.com>
2025-11-06 13:44:08 +0100

    Updated manifest and added resize error suppresion

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ root.render(
   </React.StrictMode>
 );
 
+// Suppress harmless ResizeObserver errors
+window.addEventListener('error', e => {
+  if (e.message === 'ResizeObserver loop completed with undelivered notifications.' ||
+      e.message === 'ResizeObserver loop limit exceeded') {
+    const resizeObserverErrDiv = document.getElementById('webpack-dev-server-client-overlay-div');
+    const resizeObserverErr = document.getElementById('webpack-dev-server-client-overlay');
+    if (resizeObserverErr) resizeObserverErr.setAttribute('style', 'display: none');
+    if (resizeObserverErrDiv) resizeObserverErrDiv.setAttribute('style', 'display: none');
+  }
+});
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
